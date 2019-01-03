dec7f8f8d6a452cde954b6a8a03fb4e9e9628996
Kevin Li <kevinli4321@gmail.com>
2019-01-03 18:51:08 -0500

    isObjective function

--- Block.java
+++ Block.java
@@ -2,9 +2,13 @@ public class Block {
   private boolean isVisible;
   //private item itemHere;
   //private Pokemon pokemonHere;
-  private boolean isObjective;
+  private boolean objectiveHere;
   private String type;
   private int xcor;
   private int ycor;
   private String color;
+
+  public boolean isObjective() {
+    return objectiveHere;
+  }
 }
