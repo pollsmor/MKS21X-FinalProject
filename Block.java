a5e5cfff39abace02ecf467dd248f22c57eb87ba
Kevin Li <kevinli4321@gmail.com>
2019-01-03 18:49:23 -0500

    Driver renamed to Game, Block class + instance variables

---
+++ Block.java
@@ -0,0 +1,10 @@
+public class Block {
+  private boolean isVisible;
+  //private item itemHere;
+  //private Pokemon pokemonHere;
+  private boolean isObjective;
+  private String type;
+  private int xcor;
+  private int ycor;
+  private String color;
+}
