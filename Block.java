f6cab3446d0b066b27f50cf521e436af6e2f10e8
Kevin Li <kevinli4321@gmail.com>
2019-01-03 18:57:28 -0500

    Instance variables for Pokemon class

--- Block.java
+++ Block.java
@@ -1,14 +1,26 @@
 public class Block {
   private boolean isVisible;
-  //private item itemHere;
-  //private Pokemon pokemonHere;
+  //private item itemHere; for later
+  //private Pokemon pokemonHere; for later
   private boolean objectiveHere;
-  private String type;
+  //private String type; for later
   private int xcor;
   private int ycor;
   private String color;
 
   public boolean isObjective() {
     return objectiveHere;
+  }
+
+  public void spawnEnemy() {
+
+  }
+
+  public void spawnObjective() {
+
+  }
+
+  public void spawnPlayer() {
+
   }
 }
---
+++ Pokemon.java
@@ -0,0 +1,17 @@
+public class Pokemon {
+  private String name;
+  private int level;
+  //private Type type; for later
+  private int maxHP;
+  private int currentHP;
+  private int attack;
+  private int defense;
+  //private Item[] toolbox; for later
+  //private Status[] status; for later
+  //private Move move1; for later
+  //private Move move2; for later
+  //private Move move3; for later
+  //private Move move4; for later
+
+  
+}
