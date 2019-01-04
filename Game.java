2d3da48ea7fa6120dba19e2cc1671d018c877c3f
Kevin Li <kevinli4321@gmail.com>
2019-01-03 19:56:45 -0500

    Key getter thingy doesn't work?

--- Game.java
+++ Game.java
@@ -84,6 +84,15 @@ public class Game {
 
         putString(1, 1, terminal, key + "        "); //to clear leftover letters pad withspaces
       }
+
+      //Do even when no key is pressed:
+      long tEnd = System.currentTimeMillis();
+      long millis = tEnd - tStart;
+      putString(1, 2, terminal, "Milliseconds since start of program: " + millis);
+      if (millis / 1000 > lastSecond) {
+        lastSecond = millis / 1000; //One second has passed.
+        putString(1, 3, terminal, "Seconds since start of program: " + lastSecond);
+      }
     }
   }
 }
