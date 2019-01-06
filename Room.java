581e826c4d47d0338aa5f879fe8cf8258405fbdb
Rachel Leong <rleong00@stuy.edu>
2019-01-06 01:32:52 -0500

    Fixed setVisibility() to be void

--- Room.java
+++ Room.java
@@ -1,13 +1,42 @@
-public class Room{
+
+public class Room implements Explorable{
   private boolean isExplored;
   private Block[][] blocksHere;
   private int seed;
+  private int width;
+  private int length;
 
-  public Room(){
-    Random rnd = new Random();
-    int length = Math.abs(rnd.nextInt() % 7);
-    int width = Math.abs(rnd.nextInt() % 7);
-    blocksHere = new Block[length][width];
+  //public Tunnel(Block start, Block end)
+  /**Creates a Tunnel given two blocks (start, end) beginning with the start block and ending at the end block
+    *Precondition: start is to the top and left of end block
+    *@param start is the Block representing the top left Block of the Room to be made
+    *@param end is the Block representing the bottom right Block of the Room to be made
+  */
+  public Room(Block start, Block end){
+    int xcor = start.getX();
+    int ycor = start.getY();
+    width = xcor - end.getX();
+    length = ycor - end.getY();
+    blocksHere = new Block[width][length];
+    for (int x = 0; x < length; x++){
+      for (int y = 0; y < width; y++){
+        blocksHere[x][y] = new Block(xcor+x,ycor+y,"Room");
+      }
+    }
   }
 
+  public boolean isExplored(){
+
+  }
+
+  //public void setVisibility()
+  /**Sets isExplored of all Blocks in the Room to true
+  */
+  public boolean setVisibility(){
+    for (int x = 0; x < width; x++){
+      for (int y = 0; y < length; y++){
+        blocks.setVisibility();
+      }
+    }
+  }
 }
