2f0926256d3d81cd47cdc233f5d4fb461d728f8c
Rachel Leong <rleong00@stuy.edu>
2019-01-06 01:21:25 -0500

    Fixed a typo in documentation, added comment that setVisibility will need to be edited later for optimum behavior

--- Tunnel.java
+++ Tunnel.java
@@ -8,7 +8,7 @@ public class Tunnel implements Explorable{
   /**Creates a Tunnel given two blocks (start, end) beginning with the start block and ending at the end block
     *Precondition: start and end block have either the same xcor or the same ycor
     *@param start is the Block representing the start of the Tunnel to be made
-    *@param end is the Block representing the end of the tunnel to be made
+    *@param end is the Block representing the end of the Tunnel to be made
   */
   public Tunnel(Block start, Block end){
     isExplored = false;
@@ -48,5 +48,7 @@ public class Tunnel implements Explorable{
     for (int i = 0; i < blocksHere.length; i ++){
       blocksHere[i].setVisibility();
     }
+    //To be edited later such that only the next block will be visible, not the whole tunnel
+    //Requires input from the Player
   }
 }
