e0360f2bd9885d4fb939e4d20c92aee1d50405f9
Rachel Leong <rleong00@stuy.edu>
2019-01-06 03:00:09 -0500

    Merged

--- Game.java
+++ Game.java
@@ -1,4 +1,5 @@
 //API : http://mabe02.github.io/lanterna/apidocs/2.1/
+import com.googlecode.lanterna.terminal.Terminal.SGR;
 import com.googlecode.lanterna.TerminalFacade;
 import com.googlecode.lanterna.input.Key;
 import com.googlecode.lanterna.input.Key.Kind;
@@ -13,13 +14,17 @@ import com.googlecode.lanterna.input.Key;
 import com.googlecode.lanterna.input.KeyMappingProfile;
 
 public class Game {
-  //private Pokemon[] pokemon;
-  //private Player player;
+  private Player player;
   //private Enemy[] enemies;
   private int floors;
   //private Items[] items;
   //private Mission[] missions;
   private int seed;
+  private static boolean alive = true;
+
+  public static void endGame() { //Just so I can change this in other classes, mutator method
+    alive = false;
+  }
 
   public static void putString(int r, int c, Terminal t, String s) {
     t.moveCursor(r, c);
@@ -29,30 +34,58 @@ public class Game {
   }
 
   public static void main(String[] args) {
+    Player squirtle = new Player("yoloswag");
+
     int x = 10;
     int y = 10;
     floors = 1; //Starting off on the first floor
 
-    Terminal terminal = TerminalFacade.createTerminal();
+    Terminal terminal = TerminalFacade.createTextTerminal();
     terminal.enterPrivateMode();
 
     TerminalSize terminalSize = terminal.getTerminalSize();
     terminal.setCursorVisible(false);
-    
+
     boolean running = true;
 
     long tStart = System.currentTimeMillis();
     long lastSecond = 0;
 
+    boolean running = true;
+
     while (running) {
+      //if (!alive) {
+        //running = false;
+      //}
+
       terminal.moveCursor(x, y);
-      terminal.applyBackgroundColor(Terminal.Color.YELLOW);
-      terminal.applyForegroundColor(Terminal.Color.RED);
-      terminal.putCharacter('O');
+      terminal.applyBackgroundColor(Terminal.Color.WHITE);
+      terminal.applyForegroundColor(Terminal.Color.BLACK);
+      terminal.applySGR(Terminal.SGR.ENTER_UNDERLINE);
+      terminal.putCharacter('\u00a4');
       terminal.applyBackgroundColor(Terminal.Color.DEFAULT);
       terminal.applyForegroundColor(Terminal.Color.DEFAULT);
+      terminal.applySGR(Terminal.SGR.RESET_ALL);
 
       Key key = terminal.readInput();
+
+      if (!alive) {
+        putString(0, terminalSize.getRows(), terminal,"You died. Would you like to respawn? (y/n)");
+        while (!alive) {
+          Key key2 = terminal.readInput();
+          if (key2 != null) {
+            if (key2.getCharacter() == 'y') {
+              alive = true;                                     //Below is so I can replace the you died text:
+              putString(0, terminalSize.getRows(), terminal, "                                          ");
+            }
+
+            if (key2.getCharacter() == 'n') {
+              alive = true;
+              running = false;
+            }
+          }
+        }
+      }
 
       if (key != null) {
         if (key.getKind() == Key.Kind.Escape) {
@@ -62,39 +95,47 @@ public class Game {
 
         if (key.getKind() == Key.Kind.ArrowLeft) {
           terminal.moveCursor(x, y);
-          terminal.putCharacter('P');
+          terminal.putCharacter(' ');
           --x;
         }
 
         if (key.getKind() == Key.Kind.ArrowRight) {
           terminal.moveCursor(x, y);
-          terminal.putCharacter('P');
+          terminal.putCharacter(' ');
           ++x;
         }
 
         if (key.getKind() == Key.Kind.ArrowUp) {
           terminal.moveCursor(x, y);
-          terminal.putCharacter('P');
+          terminal.putCharacter(' ');
           --y;
         }
 
         if (key.getKind() == Key.Kind.ArrowDown) {
           terminal.moveCursor(x, y);
-          terminal.putCharacter('P');
+          terminal.putCharacter(' ');
           ++y;
+        }
+
+        if (key.getKind() == Key.Kind.Backspace) {
+          squirtle.faintPlayer();
         }
 
         putString(1, 1, terminal, key + "        "); //to clear leftover letters pad withspaces
       }
 
-      //Do even when no key is pressed:
+      //Do even when no key is pressed:ßß
       long tEnd = System.currentTimeMillis();
       long millis = tEnd - tStart;
-      //putString(1, 2, terminal, "Milliseconds since start of program: " + millis);
-      //if (millis / 1000 > lastSecond) {
-      //  lastSecond = millis / 1000; //One second has passed.
-      //  putString(1, 3, terminal, "Seconds since start of program: " + lastSecond);
-      //}
+      putString(1, 2, terminal, "Milliseconds since start of program: " + millis);
+      if (millis / 1000 > lastSecond) {
+        lastSecond = millis / 1000; //One second has passed.
+        putString(1, 3, terminal, "Seconds since start of program: " + lastSecond);
+      }
     }
+
+    terminal.exitPrivateMode();
+    System.out.println("Thank you for playing Pokemon Terminal Dungeon.");
+    System.exit(0);
   }
 }
