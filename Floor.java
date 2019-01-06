b44c5fdad4c1e3bd0d183b5cdde4d9416464bc44
Rachel Leong <rleong00@stuy.edu>
2019-01-06 01:54:23 -0500

    Added constructor and getFloor()

--- Floor.java
+++ Floor.java
@@ -1,4 +1,14 @@
 public class Floor{
   private Block[][] blocksHere; //Array of blocks (like patches)
-  
+  private int floorNumber;
+  //private Mission[] missions; to be added later if we get to it
+
+  public Floor(int num, int terminalWidth, int terminalLength){
+    floorNumber = num;
+    blocksHere = new Block[terminalWidth][terminalLength];
+  }
+
+  public int getFloor(){
+    return floorNumber;
+  }
 }
