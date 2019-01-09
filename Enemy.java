63f93d26bbd4ded372b9668c080e7358f2128498
Kevin Li <kevinli4321@gmail.com>
2019-01-08 20:46:54 -0500

    Enemy class comments

--- Enemy.java
+++ Enemy.java
@@ -1,14 +1,16 @@
 public class Enemy extends Pokemon {
   private int killXP;
 
+  //same constructor as player but this allows custom values
   public Enemy(String name, int level, int maxHP, int attack, int defense) {
     super(name, level, maxHP, attack, defense);
   }
 
+  //add this XP to the player when the monster is killed
   public int getKillXP() {
     return killXP;
   }
-
+  
   public void atkPlayer(Player player, int damage) {
     player.setHP(-1 * damage);
   }
