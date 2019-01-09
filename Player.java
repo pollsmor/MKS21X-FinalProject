587ad5e51e613d97956155c3a0779111dfbcc7f4
Kevin Li <kevinli4321@gmail.com>
2019-01-08 20:49:54 -0500

    Player class comments

--- Player.java
+++ Player.java
@@ -3,14 +3,16 @@ public class Player extends Pokemon {
   private int currentHunger;
   private int xp;
 
+  //In the core games the starter is typically lvl 5, 20 HP, and around 10 atk/def
   public Player(String name) {
-    super(name, 5, 20, 5, 5);
+    super(name, 5, 20, 10, 10);
   }
 
   public int getHunger() {
     return currentHunger;
   }
 
+  //Calls static fn. in Game to end inner while loop in Game
   public void faintPlayer() {
     Game.endGame();
   }
