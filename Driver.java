960a00337fca7419b068855e0e5f8c99c70ff482
Kevin Li <kevinli4321@gmail.com>
2019-01-10 11:39:28 -0500

    Moved everything from Game to Driver

--- Driver.java
+++ Driver.java
@@ -43,8 +43,6 @@ public class Driver {
     long tStart = System.currentTimeMillis();
     long lastSecond = 0;
 
-    boolean running = true;
-
     //I don't want this to be try-ed in case the user only provides 1 input (which is valid)
     if (args.length >= 2) {
       try {
@@ -57,5 +55,103 @@ public class Driver {
         System.exit(0);
       }
     }
+
+    String name = args[0];
+    int seed = Integer.parseInt(args[1]);
+    Game game;
+    boolean running = true;
+    boolean alive = true; //controls the inner while loop
+
+    //Instantiate game outside of try since it wouldn't work inside it
+    if (args.length == 1)
+      game = new Game(name, width, length);
+
+    else {
+      game = new Game(name, seed, width, length);
+    }
+
+    while (running) {
+      terminal.moveCursor(x, y);
+      //Applying background makes it look bad, can't see the symbol as easily
+      //terminal.applyBackgroundColor(Terminal.Color.WHITE);
+      terminal.applyForegroundColor(Terminal.Color.GREEN); //Green is nice, right?
+      terminal.applySGR(Terminal.SGR.ENTER_UNDERLINE);
+      terminal.putCharacter('\u04dd'); //was '\u00a4'
+      terminal.applyBackgroundColor(Terminal.Color.DEFAULT);
+      terminal.applyForegroundColor(Terminal.Color.DEFAULT);
+      terminal.applySGR(Terminal.SGR.RESET_ALL);
+
+      Key key = terminal.readInput();
+
+      if (!alive) {
+        putString(2, width, terminal,"You died. Would you like to respawn? (y/n)");
+        while (!alive) {
+          Key key2 = terminal.readInput(); //if I'm in the if I can't read the first readInput, so I need a second one
+          if (key2 != null) {
+            //Do you want to respawn?
+            if (key2.getCharacter() == 'y') {
+              alive = true;                //Below is so I can replace the you died text:
+              putString(2, width, terminal, "                                          ");
+            }
+
+            //No, I don't want to respawn.
+            if (key2.getCharacter() == 'n') {
+              alive = true;
+              running = false;
+            }
+          }
+        }
+      }
+
+      if (key != null) {
+        if (key.getKind() == Key.Kind.Escape) {
+          terminal.exitPrivateMode();
+          System.exit(0);
+        }
+
+        if (key.getKind() == Key.Kind.ArrowLeft) {
+          terminal.moveCursor(x, y);
+          terminal.putCharacter(' ');
+          --x;
+        }
+
+        if (key.getKind() == Key.Kind.ArrowRight) {
+          terminal.moveCursor(x, y);
+          terminal.putCharacter(' ');
+          ++x;
+        }
+
+        if (key.getKind() == Key.Kind.ArrowUp) {
+          terminal.moveCursor(x, y);
+          terminal.putCharacter(' ');
+          --y;
+        }
+
+        if (key.getKind() == Key.Kind.ArrowDown) {
+          terminal.moveCursor(x, y);
+          terminal.putCharacter(' ');
+          ++y;
+        }
+
+        if (key.getKind() == Key.Kind.Backspace) {
+          alive = false;
+        }
+
+        putString(1, 1, terminal, key + "        "); //to clear leftover letters pad withspaces
+      }
+
+      //Do even when no key is pressed:ßß
+      long tEnd = System.currentTimeMillis();
+      long millis = tEnd - tStart;
+      putString(1, 2, terminal, "Milliseconds since start of program: " + millis);
+      if (millis / 1000 > lastSecond) {
+        lastSecond = millis / 1000; //One second has passed.
+        putString(1, 3, terminal, "Seconds since start of program: " + lastSecond);
+      }
+    }
+
+    terminal.exitPrivateMode();
+    System.out.println("Thank you for playing Pokemon Terminal Dungeon.");
+    System.exit(0);
   }
 }
