e829f2241ee230819666a1ba290e210297ba7d6e
rleongPd5 <rleong00@stuy.edu>
2019-01-13 16:48:59 -0500

    Commented out some test cases

--- FloorDriver.java
+++ FloorDriver.java
@@ -38,7 +38,7 @@ public class FloorDriver{
     System.out.println(y.toString());*/
 
     //Testing tooClose
-    Room roomOriginal = new Room(4, 4, 7, 7);
+    //Room roomOriginal = new Room(4, 4, 7, 7);
     /*System.out.println("\tResult: "+roomOriginal.tooClose(10, 10, 11, 11)); //Should print false
     System.out.println("\tResult: "+roomOriginal.tooClose(1, 1, 3, 3)); //Should print true
     System.out.println("\tResult: "+roomOriginal.tooClose(2, 4, 6, 6)); //Should print true
@@ -75,38 +75,38 @@ public class FloorDriver{
     */
 
     //Testing constructor Tunnel(int startXcor, int startYcor, int newLength, boolean isHorizontal)
-    Tunnel tuna = new Tunnel(5, 5, 2, true, true, a);
-    System.out.println(tuna.toString()+"\n");
+    //Tunnel tuna = new Tunnel(5, 5, 2, true, true, a);
+    //System.out.println(tuna.toString()+"\n");
     /*System.out.println(tuna.getLength());
     System.out.println(tuna.getStartBlock().toString());
     System.out.println(tuna.getEndBlock().toString());*/
-    Tunnel salmon = new Tunnel(9, 9, 5, false, false, a);
-    System.out.println(salmon.toString()+"\n");
+    //Tunnel salmon = new Tunnel(9, 9, 5, false, false, a);
+    //System.out.println(salmon.toString()+"\n");
 
-    Tunnel tilapia = new Tunnel(10, 7, 4, true, false, a);
-    System.out.println(tilapia.toString() +"\n");
+    //Tunnel tilapia = new Tunnel(10, 7, 4, true, false, a);
+    //System.out.println(tilapia.toString() +"\n");
 
-    Tunnel cod = new Tunnel( 8, 2, 3, false, true, a);
-    System.out.println(cod.toString() +"\n");
+    //Tunnel cod = new Tunnel( 8, 2, 3, false, true, a);
+    //System.out.println(cod.toString() +"\n");
 
-    Tunnel clownfish = new Tunnel (1, 1, 1, false, true, a);
-    System.out.println(clownfish.toString()+"\n");
+    //Tunnel clownfish = new Tunnel (1, 1, 1, false, true, a);
+    //System.out.println(clownfish.toString()+"\n");
 
     //Testing constructor Tunnel(Block start, Block end)
-    Block bleh = new Block(9, 9,"Tunnel");
-    Block blah = new Block (9, 10, "Tunnel");
-    Tunnel bluh = new Tunnel(bleh, blah, a);
-    System.out.println(bluh.getLength());
-    System.out.println(bluh.toString());
-    System.out.println();
-    Block starter = new Block(0,0,"Tunnel");
-    Block ender = new Block(1,0,"Tunnel");
+    //Block bleh = new Block(9, 9,"Tunnel");
+    //Block blah = new Block (9, 10, "Tunnel");
+    //Tunnel bluh = new Tunnel(bleh, blah, a);
+    //System.out.println(bluh.getLength());
+    //System.out.println(bluh.toString());
+    //System.out.println();
+    //Block starter = new Block(0,0,"Tunnel");
+    //Block ender = new Block(1,0,"Tunnel");
     //System.out.println(starter.getData());
-    Tunnel fish = new Tunnel(starter, ender, a);
-    System.out.println(fish.getLength());
+    //Tunnel fish = new Tunnel(starter, ender, a);
+    //System.out.println(fish.getLength());
     //System.out.println(fish.getStartBlock().toString());
     //System.out.println(fish.getEndBlock().toString());
-    System.out.println(fish.toString());
+    //System.out.println(fish.toString());
     //Testing nextToRoom
 
   }
